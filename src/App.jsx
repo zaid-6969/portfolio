531fa720b30feebea4420304e257bd079134cc03
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-05-12 16:43:44 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-9xl'>Welcome to Zaid & Kaif App </div>
+    <div className='text-9xl'>Welcome to Zaid & Kaif portfolio</div>
   )
 }
 
