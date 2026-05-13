5d61b4dc889ae3bc07e6c2aa4a500119fe9756d3
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-05-13 11:02:26 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
-    <div className='text-9xl'>Welcome to Zaid & Kaif portfolio</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
