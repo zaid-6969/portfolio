36884e3496237b1836b5317a44388ed05fbdd230
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-05-15 12:25:56 +0530

    theme toggle complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 import Home from "./pages/Home";
 import ProjectDetails from "./pages/ProjectDetails";
-import AdminLogin from "./pages/AdminLogin"
+import AdminLogin from "./pages/AdminLogin";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const { mode } = useSelector((s) => s.theme);
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", mode);
+  }, [mode]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
